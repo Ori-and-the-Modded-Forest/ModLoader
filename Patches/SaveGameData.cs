2ef10c88e944c30a19156220f408633c963423f5
Isabel S <willthegamedesigner@gmail.com>
2023-10-12 23:25:38 -0600

    Commenting out Save Data, doesn't work right now

--- Patches/SaveGameData.cs
+++ Patches/SaveGameData.cs
@@ -9,29 +9,29 @@ internal class patch_SaveGameData : SaveGameData {
 
 	public string World { get; private set; }
 
-	public extern void orig_SaveToWriter(BinaryWriter writer);
-	public extern bool orig_LoadFromReader(BinaryReader reader);
+	//public extern void orig_SaveToWriter(BinaryWriter writer);
+	//public extern bool orig_LoadFromReader(BinaryReader reader);
 
-	public void SetModDefaults() {
-		World = "Nibel";
-	}
+	//public void SetModDefaults() {
+	//	World = "Nibel";
+	//}
 
-	public new void SaveToWriter(BinaryWriter writer) {
-		orig_SaveToWriter(writer);
-	}
+	//public new void SaveToWriter(BinaryWriter writer) {
+	//	orig_SaveToWriter(writer);
+	//}
 
-	public new bool LoadFromReader(BinaryReader reader) {
+	//public new bool LoadFromReader(BinaryReader reader) {
 
-		if (!orig_LoadFromReader(reader))
-			return false;
+	//	if (!orig_LoadFromReader(reader))
+	//		return false;
 
-		if (reader.ReadString() != "ModdedSaveData") {
-			SetModDefaults();
-			return true;
-		}
+	//	if (reader.ReadString() != "ModdedSaveData") {
+	//		SetModDefaults();
+	//		return true;
+	//	}
 
 
-		return true;
-	}
+	//	return true;
+	//}
 }
 
